14a2e00671bde633e58d6a43ecfa65973118d8d1
Charles <qw1020046@gmail.com>
2022-11-15 20:15:42 +0800

    first commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest{
+    @Test
+    public void t1(){
+        System.out.println("hello");
+    }
+}
