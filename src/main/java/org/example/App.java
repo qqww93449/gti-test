73a083ac97ab1d97e2483695014276041f089af1
Charles <qw1020046@gmail.com>
2022-11-16 16:51:28 +0800

    Hot-fix test commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,5 +11,6 @@ public class App {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hot-fix");
+        System.out.println("Hot-fix test");
     }
 }
