96ec8d60a78a560cd77ded3b14152bf12dc1d032
Charles3387 <116353908+Charles3387@users.noreply.github.com>
2022-11-16 19:00:19 +0800

    pull commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,5 +15,6 @@ public class App {
         System.out.println("master test");
         System.out.println("Hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
